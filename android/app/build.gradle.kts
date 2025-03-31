2a89f85e7f1997c17d3d0a8f31769571207c6187
Adarsh Kumar <adarsh.mca23@gmail.com>
2025-03-31 18:55:12 +0530

    final

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.ai_doc"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.2.12479018"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
