72a78e68d3eba114f581dee47c0e29cbf17f8c67
yr1404 <yash06765@gmail.com>
2025-04-04 21:57:37 +0530

    Fixed API key exposed on Github issue

--- lib/utils/const.dart
+++
@@ -1,7 +0,0 @@
-// This file contains sensitive information and should not be tracked in git
-class AppConstants {
-  static const String geminiApiKey = 'AIzaSyDPGI-7rVgXUEyiJn3RsFbyarDMcLppoBw';
-  static const String appId = '0d2e0718e15e4be28fe26df1d6756c3e';
-  static const String token = '';
-
-}
